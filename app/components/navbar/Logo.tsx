844a29d94429b4067fa216dce88e53d64406c448
Dawar-codes <dawar.malik29@gmail.com>
2025-04-08 17:31:36 +0500

    add logo on mobile view

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -6,15 +6,27 @@ import { useRouter } from "next/navigation";
 const Logo = () => {
   const router = useRouter();
   return (
-    <Image
-    onClick={() => router.push('/')}
-      alt="logo"
-      className="hidden md:block cursor-pointer w-auto h-auto"
-      width={100}
-      height={50}
-      src="/images/airbnby.png"
-      priority
-    />
+    <>
+      {/* Mobile logo */}
+      <Image
+        onClick={() => router.push("/")}
+        alt="Mobile Logo"
+        className="block md:hidden cursor-pointer w-auto h-auto"
+        width={35}
+        height={35}
+        src="/images/airbnbyNoText.png" // your mobile logo here
+        priority
+      />
+      <Image
+        onClick={() => router.push("/")}
+        alt="logo"
+        className="hidden md:block cursor-pointer w-auto h-auto"
+        width={100}
+        height={50}
+        src="/images/airbnby.png"
+        priority
+      />
+    </>
   );
 };
 
